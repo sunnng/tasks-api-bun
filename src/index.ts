4e478e3955efd56a451c1afdc0253d1d0d977999
Scy <chengyuan7766@gmail.com>
2024-12-09 16:48:06 +0800

    error handler & not found handler build

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
-import { Hono } from 'hono'
+import type { ErrorHandler, NotFoundHandler } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
 
-const app = new Hono()
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { INTERNAL_SERVER_ERROR, NOT_FOUND, OK } from "stoker/http-status-codes";
+import { NOT_FOUND as NOT_FOUND_PHRASES } from "stoker/http-status-phrases";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new OpenAPIHono();
 
-export default app
+const notFound: NotFoundHandler = (c) => {
+  return c.json({
+    message: `not found ${NOT_FOUND_PHRASES}`,
+    NOT_FOUND,
+  });
+};
+
+const onError: ErrorHandler = (err, c) => {
+  const currentStatus = "status" in err
+    ? err.status
+    : c.newResponse(null).status;
+
+  const statusCode = currentStatus !== OK
+    ? (currentStatus as StatusCode)
+    : INTERNAL_SERVER_ERROR;
+
+  console.log(currentStatus, statusCode);
+  return c.json({
+    err: err.message,
+    statusCode,
+  });
+};
+
+// 404 handler
+app.notFound(notFound);
+
+// error handler
+app.onError(onError);
+
+app.get("/", (c) => {
+  return c.text("Hello Zod Open API Hono!");
+});
+
+app.get("/error", (c) => {
+  throw new Error("oh no!");
+});
+
+const port = 3000;
+
+// eslint-disable-next-line no-console
+console.log(`Server is running on ${port}`);
+
+export default {
+  port,
+  fetch: app.fetch,
+};
